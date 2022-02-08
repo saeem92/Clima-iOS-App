24dfb68bd1e2039249f425c45d9971f7fc3873dc
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-09 03:19:37 +0530

    Initial Commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Saeem on 09/02/2022.
+//  Copyright © 2022 Saeem. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
