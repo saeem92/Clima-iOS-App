6c4976a5c22e32a334eb0558ad0cb8b15e2dbdb8
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-14 00:33:58 +0530

    Added EndEditing Feature

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -30,11 +30,16 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         // By adding UITextFieldDelegate above and searchTextField.delegate = self , this is allowing us to do these two steps is we can now create a method called textfieldShouldReturn.....
     }
     @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        // Here we are using the endediting so when we click on search button dismiss our current action or keyboard.
         print(searchTextField.text!)
         //searchTextField.text is a property which corresponds to the text displayed by the textfield.
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        searchTextField.endEditing(true)
+        // Here we are using the endediting above so when we click on search button dismiss our current action or keyboard.
         print(searchTextField.text!)
         return true
         // We are returning true here because this function expects a boolean as an output so that our textfield will be allowed to actually return.
@@ -42,5 +47,30 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     // The textFieldShouldReturn function will tell the viewcontroller that user has pressed the return/go key on the keyboard what should we do and this will happen at the exact time when return button gets pressed.
     // By adding UITextFieldDelegate above and searchTextField.delegate = self , this is allowing us to do these two steps is we can now create a method called textfieldShouldReturn.....
     
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        // IMPORTANT below I have actually just referred searchTextField as textField this is happening because we aren't the ones triggering or calling any of these functions nowhere in our code we specially wrote these textFieldShouldEndEditing function because they are triggered by textField and became available for us to use.
+        // The UItextField class is actually the one that's responsible for calling these function/methods and when it calls textFieldShouldEndEditing basically when it detects user dismissing the keyboard, IN THAT MOMENT IT'S GONNA PASS A REFERENCE TO THE textField that triggered this method.
+        
+        // We could for example have a multiple textfield similat to how in our IBAciton we can get hold of the sender which was the UIButton that actually triggered this IBAction by detecting a touchevent.
+        
+        // We can have multiple textfield or triggering the same methods so if we had multiple buttons linked to the same IBAction then any of those buttons could be the sender it just depends on who detected the event.
+        
+        // IF WE HAVE MULTIPLE textFields ALL WITH THEIR DELEGATES SET AS THIS CURRENT CLASS ANY OF THOSE textFields COULD BE THE TRIGGER FOR THOSE DELEGATE METHODS BUT THEY WILL PASS THEIR IDENTITY IN THROUGH THIS TextField parameter so if we dont care about which textfield was triggered then we can simply use this parameter (_ textField: UITextField) and apply our validation to any textField in our currentViewController using this code
+        
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    // In the above function we are checking if user hasn't typed anything we will display him a placeholder saying type something if the user has typed something then it will work normally keyboard will dissppear and we will get our desired result.
+    // The above function gets to decide what to do when the user clicked on go or return without typing in the search field.
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Use searchTextField.text to get the weather for that city.
+        searchTextField.text = ""
+    }
+    // The above function is used to stop editing and clear the search when we have finsished editing or entering a city name and when we hit search or go it's gonna clear the search label so that we can enter a new city name next time.
 }
 
